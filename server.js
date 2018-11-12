6f45c202782ca121adbbcb9cf3e296930da6a1e4
DouglasMarianoSilverio <douglas07@gmail.com>
2018-11-12 09:13:53 -0200

    aula 3 - servidor rodando

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 'use strict'
-console.log('testando.....');
+
+
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+//sempre que for importar com algo da aplicação começar com ./ no require ex: ('./meu arquivo')
+
+//vamos usar mvc ;)
+const app = express();
+const port = 3000;
+app.set('port',port);
+
+const server = http.createServer(app);
+const router = express.Router();
+//rota padrão
+const route = router.get('/', (req,res,netx) =>{
+     res.status(200).send({
+         title: "Node Store API",
+         version: "0.0.1"
+     })
+}) ;
+
+app.use('/', route);
+
+server.listen(port);
+console.log('API rodando na porta ' + port);
